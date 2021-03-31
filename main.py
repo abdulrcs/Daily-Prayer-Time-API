6167344cf4aca9ccb7978d0dd4baba2a5ecf3fbf
Abdul Rahman <54136956+abdulrcs@users.noreply.github.com>
2021-03-31 15:37:49 +0800

    :hammer: fix edge case for tomorrow prayers

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
-from bs4 import BeautifulSoup
 from flask import Flask, jsonify
 from threading import Thread
 import json
 from os import system
 import googlesearch
+from bs4 import BeautifulSoup
+import dateparser
 app = Flask('')
 app.config['JSON_SORT_KEYS'] = False
 
@@ -31,12 +32,21 @@ def prayer(s):
     data["tomorrow"] = {}
     waktu = soup.find_all("span", attrs ={"class": "waktu-solat"})
     jam = soup.find_all("span", attrs ={"class": "jam-solat"})
-    names = ["Date","Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha'a"]
-    tomorrow = soup.find("tr", attrs={"class": "active"}).find_next("tr").find_all("td", attrs={"class": "prayertime"})
     for x,y in zip(waktu,jam):
       data["today"][x.get_text()] = y.get_text()
-    for x,y in zip(names,tomorrow):
-      data["tomorrow"][x] = y.get_text()
+    names = ["Date","Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha'a"]
+    try:
+      tomorrow = soup.find("tr", attrs={"class": "active"}).find_next("tr").find_all("td", attrs={"class": "prayertime"})
+      for x,y in zip(names,tomorrow):
+        data["tomorrow"][x] = y.get_text()
+    except :
+      month = str(dateparser.parse(data["date"]))[5:7]
+      url = url + '?date=2021-' + str(int(month)+1)
+      response = requests.get(url)
+      soup = BeautifulSoup(response.content, "html.parser")
+      tomorrow = soup.find_all("tr")[1].find_all("td", attrs={"class": "prayertime"})
+      for x,y in zip(names,tomorrow):
+        data["tomorrow"][x] = y.get_text()
   except :
     data["Error"] = "Result Not Found"
   return jsonify(data)
